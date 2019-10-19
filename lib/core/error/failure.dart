2a3e9c28ec9535bff9aeb0ea7085995dd26e7d96
Sayeed <sayeedhussain19@gmail.com>
2019-10-19 11:56:11 +0530

    [Sayeed] Add cache policy

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -6,3 +6,7 @@ class Failure {
 class APIFailure extends Failure {
   APIFailure({String message}) : super(message: message);
 }
+
+class CacheFailure extends Failure {
+  CacheFailure({String message}) : super(message: message);
+}
