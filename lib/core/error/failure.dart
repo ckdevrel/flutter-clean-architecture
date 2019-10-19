22d1318a98fea233636302d4aad00a7ca27b0418
Sayeed <sayeedhussain19@gmail.com>
2019-10-19 05:49:41 +0530

    [Sayeed] Add repository implementation with mock remote datasource

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,1 +1,8 @@
-class Failure  {}
+class Failure {
+  Failure({this.message});
+  final String message;
+}
+
+class APIFailure extends Failure {
+  APIFailure({String message}) : super(message: message);
+}
--- lib/features/feed/data/feed_remote_data_source.dart
+++ lib/features/feed/data/feed_remote_data_source.dart
@@ -1,5 +1,6 @@
+import 'package:flutter_clean_architecture/features/feed/data/feed_item_model.dart';
 import 'package:flutter_clean_architecture/features/feed/domain/entities/feed_item.dart';
 
 abstract class FeedRemoteDataSource {
-  Future<List<FeedItem>> getFeed();
+  Future<List<FeedItemModel>> getFeed();
 }
