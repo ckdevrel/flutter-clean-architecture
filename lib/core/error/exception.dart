4fe8dad458e8f94a2df20e6e963f5bebabd26651
Sayeed <sayeedhussain19@gmail.com>
2019-10-19 05:49:41 +0530

    [Sayeed] Add repository implementation with mock remote datasource

---
+++ lib/core/error/exception.dart
@@ -0,0 +1,1 @@
+class APIException implements Exception {}
---
+++ lib/features/feed/domain/repositories/feeds_respository_impl.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_clean_architecture/core/error/failure.dart';
+import 'package:flutter_clean_architecture/features/feed/data/feed_remote_data_source.dart';
+import 'package:flutter_clean_architecture/features/feed/domain/entities/feed_item.dart';
+import 'package:flutter_clean_architecture/features/feed/domain/repositories/feeds_repository.dart';
+
+class FeedsRespositoryImpl implements FeedsRepository {
+  FeedsRespositoryImpl({@required this.remoteDataSource});
+
+  final FeedRemoteDataSource remoteDataSource;
+
+  @override
+  Future<Either<Failure, List<FeedItem>>> getFeeds() async {
+    try {
+      final feed = await remoteDataSource.getFeed();
+      return Right(feed);
+    } catch (e) {
+      return Left(APIFailure(message: 'API Error'));
+    }
+  }
+}
