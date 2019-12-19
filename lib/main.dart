d79b5da8168afef81207dd5682949f9df36a3b4b
TakeoffAndroid <takeoffandroid@gmail.com>
2019-12-19 14:41:26 +0530

    Fixes blank screen issue

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'features/feed/presentation/screens/feeds_screen.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await di.init();
   runApp(MyApp());
 }
